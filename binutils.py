4d2caf126788c76b5ac9812681fe6f450e273946
Jason Carnevale <jcarneva@cisco.com>
2013-05-04 02:22:06 -0700

    renamed iputils.py to binutils.py, added 2 static methods to binutils class

---
+++ binutils.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+''' 
+binutils.py - Mixin class that provides basic utilities for the ipaddr library. 
+Only utilities that are not specific to any addressing space should be added
+to this class.
+'''
+import re, bitstring
+
+class IpUtils(object):
+	''' Mixin class containing basic conversion utilies that are not
+	specific to any addressing scheme. '''
+	
+	@staticmethod
+	def addressHex(address):
+		''' Convert an address from bitstring to hexidecimal string 
+		>>>> iputils.addressHex('0b11010011')
+		'0xD3'
+		'''
+		try:
+			return hex(int(address, 2))
+		except ValueError as e:
+			return e
+		except TypeError as e:
+			return e
+
+	@staticmethod
+	def addressBin(address):
+		''' Convert an address from hexidecimal string or int to
+		bitstring.
+
+		>>>> iputils.addressBin('0xD3')
+		'0b11010011'
+		>>>> iputils.addressBin(6123)
+		'0b00000000000000000001011111101011'
+		'''
+
+		try:
+			if isinstance(address, int):
+				return bin(int(address))
+			else:
+				if address[0:2] == '0b':
+					return address
+				elif address[0:2] == '0x':
+					return bin(int(address, 16))
+				else:
+					raise ValueError
+		except ValueError as e:
+			return e
+
+
+
+
