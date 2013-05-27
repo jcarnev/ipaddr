529ba7a53162dddce986d7a099ef89afb4368866
Jason Carnevale <jcarneva@cisco.com>
2013-05-27 10:10:55 -0700

    cleaning up directory

--- ipaddr.py
+++ ipaddr.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+    # IPV4 Address and Network subnetting library
+    # Copyright (C) 2013  Jason Carnevale
+
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import re
 
 # factory function to parse different IPv4 notations and return the appropriate class
